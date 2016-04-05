1cf399660fbfba54fcff4706c5e0b81006447faa
Simon <simon@Kaitlyn.local>
2016-04-05 18:31:33 +1200

    Latest documentation run

--- ideannotator/elementlist.js
+++ ideannotator/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Annotatable"],["c","AnnotatePermissionChecker"],["c","DataObjectAnnotator"],["c","DataObjectAnnotatorTask"]];
+ApiGen.elements = [["c","Annotatable"],["c","AnnotateClassInfo"],["c","AnnotatePermissionChecker"],["c","DataObjectAnnotator"],["c","DataObjectAnnotatorTask"],["c","DocBlockGenerator"],["c","DocBlockTagGenerator"]];
