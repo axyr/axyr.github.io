e2b3ab4c3103a03bc77fe18eac2027faa4836f75
Simon <simon@casa-laguna.net>
2016-04-14 19:22:37 +1200

    prepping the docs for V2 release

--- ideannotator/elementlist.js
+++ ideannotator/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Annotatable"],["c","AnnotateClassInfo"],["c","AnnotatePermissionChecker"],["c","DataObjectAnnotator"],["c","DataObjectAnnotatorTask"],["c","DocBlockGenerator"],["c","DocBlockTagGenerator"]];
+ApiGen.elements = [["c","AbstractTagGenerator"],["c","Annotatable"],["c","AnnotateClassInfo"],["c","AnnotatePermissionChecker"],["c","ControllerTagGenerator"],["c","DataObjectAnnotator"],["c","DataObjectAnnotatorTask"],["c","DocBlockGenerator"],["c","OrmTagGenerator"]];
